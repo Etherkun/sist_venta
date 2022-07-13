ee8e1f6faf7a16ac78950a90b44f3d6eda1a9a47
Etherkun <rodomax6@gmail.com>
2022-07-13 14:02:03 -0400

    new file:   database/seeders/Userseeder.php 	new file:   resources/views/sistema/reporte/index.blade.php

---
+++ database/seeders/Userseeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+class Userseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+/*         User::create([
+
+            'name' => 'Rodolfo Pace',
+            'email' => 'rodomax6@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+
+        ])->assignRole($admin); */
+    }
+}
